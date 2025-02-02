e9785dea8205ed4b3b0f0d1f30d2cb194cee9793
ThienNguyen3001 <thiennguyen03001@gmail.com>
2025-02-02 12:24:49 +0700

    minor changes

--- Elevator/Program.cs
+++ Elevator/Program.cs
@@ -12,13 +12,16 @@ namespace Elevator
         static void InputFromArr()
         {
             Elevator elevator = new Elevator(3);
+
+            // Test case
             //string[] a = { "2U", "3D", "5U", "1" };
             //string[] a = { "5D", "3D" };
             //string[] a = { "3U", "5D" };
             //string[] a = { "4D", "5D" };
             //string[] a = { "2U", "3D", "3U", "2D", "5D", "5U", "4D" };
-            //string[] a = { "2U", "3D", "3U", "2D", "5D", "5U", "4D" };
-            string[] a = { "3U", "5D", "6", "4U", "6", "4D", "5U", "1", "2D", "1" };
+            string[] a = { "2D", "3D", "3U", "2D", "5D", "5U", "4U" };
+            //string[] a = { "3U", "5D", "6", "4U", "6", "4D", "5U", "1", "2D", "1" };
+
             for (int i = 0; i < a.Length; i++)
             {
                 elevator.Insert(a[i]);
